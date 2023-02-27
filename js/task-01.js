53f3c7cdf7470003396df66ce961b505f08a7a01
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-02-27 10:45:31 +0200

    Solving task #1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+
+const items = document.querySelectorAll('.item');
+console.log('Number of categories:', items.length);
+
+items.forEach(el => {
+    console.log('');
+    console.log('Category:', el.querySelector('h2').textContent);
+    console.log('Elements:',  el.querySelectorAll('li').length);
+});
