72a65d4a9c6d7754988c9da5910d44da056b1ad3
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-02-27 10:59:45 +0200

    Solving task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+ingredients.forEach(el => {
+  const li = document.createElement('li');
+  li.textContent = el;
+  li.classList.add('.item');
+  list.appendChild(li);
+});
