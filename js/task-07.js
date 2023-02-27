e66a899b794776d917c753c373bcb66b9ad80dfe
Serhii-K5 <kch_i@i.ua>
2023-02-27 19:44:29 +0200

    Solving task #7

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+
+const elInput = document.querySelector("#font-size-control");
+const txtSpan = document.querySelector("#text");
+
+// v1
+elInput.addEventListener("change", (eventChange) => 
+  (txtSpan.style.fontSize = eventChange.target.value + "px")
+);
+
+// v2
+// elInput.addEventListener("change", (eventChange) => 
+//   (txtSpan.style.fontSize = `${eventChange.target.value}px`)
+// );
