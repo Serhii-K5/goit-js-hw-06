2515daafc01fcb86f27a65ebba0bf8503c1a03c5
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-02-28 16:02:53 +0200

    Solving task #10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputTxt = document.querySelector('#controls input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+const createBoxes = amount => {
+	const arrDiv = [];
+  for (let number = 0; number < amount; number++) {
+		const divEl = document.createElement('div');
+    divEl.style.height = divEl.style.width = (30 + 10 * number) + 'px';
+		divEl.style.background = getRandomHexColor();
+		arrDiv.push(divEl);
+	}
+	return arrDiv;
+}
+
+btnCreate.addEventListener('click', () => 
+  boxesDiv.append(...createBoxes(inputTxt.value))
+);
+
+btnDestroy.addEventListener('click', () => boxesDiv.innerHTML = '');
