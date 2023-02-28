b8d5890a2a7cacf08377e5bdcbc8472abeec7827
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-02-28 14:33:21 +0200

    Solver task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('button.change-color');
+const spanColor = document.querySelector('span.color');
+
+btnChangeColor.addEventListener('click', () => 
+  body.style.backgroundColor = spanColor.textContent = getRandomHexColor()
+)
